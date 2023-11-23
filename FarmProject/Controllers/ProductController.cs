4cb05f065fe7b3214f6ef3b0a69ecfe2babdb968
Abdul Latif <a.latif@ndessl.local>
2023-11-23 20:54:28 +0600

    Products Added without Get

---
+++ FarmProject/Controllers/ProductController.cs
@@ -0,0 +1,138 @@
+﻿using FarmProject.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+
+namespace FarmProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        private readonly string ConnectionString;
+
+        public ProductController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            ConnectionString = _configuration.GetConnectionString("DefaultConnection");
+        }
+        [HttpPost("Createproduct")]
+        public IActionResult CreateSupplier(Product product)
+        {
+            try
+            {
+                using (SqlConnection con = new SqlConnection(ConnectionString))
+                {
+                    con.Open();
+
+                    SqlCommand checkCmd = new SqlCommand("SELECT COUNT(*) FROM Products WHERE ProductName = @ProductName", con);
+                    checkCmd.Parameters.AddWithValue("@ProductName", product.ProductName);
+                    int count = (int)checkCmd.ExecuteScalar();
+
+                    if (count > 0)
+                    {
+                        return BadRequest(new { message = "Product already exists" });
+                    }
+                    else
+                    {
+                        using (SqlCommand insertCmd = new SqlCommand("INSERT INTO Products (ProductName, ProductDescription, Status, Price, CategoryId, UomId, AddedBy, AddedDate, AddedPc, UpdatedBy, UpdatedDate, UpdatedPc) VALUES (@ProductName, @ProductDescription, @Status, @Price, @CategoryId, @UomId, @AddedBy, @AddedDate, @AddedPc, @UpdatedBy, @UpdatedDate, @UpdatedPc)", con))
+                        {
+                            insertCmd.Parameters.AddWithValue("@ProductName", product.ProductName);
+                            insertCmd.Parameters.AddWithValue("@ProductDescription", product.ProductDescription ?? (object)DBNull.Value);
+                            insertCmd.Parameters.AddWithValue("@Status", product.Status);
+                            insertCmd.Parameters.AddWithValue("@Price", product.Price ?? (object)DBNull.Value);
+                            insertCmd.Parameters.AddWithValue("@CategoryId", product.CategoryId ?? (object)DBNull.Value);
+                            insertCmd.Parameters.AddWithValue("@UomId", product.UomId ?? (object)DBNull.Value);
+                            insertCmd.Parameters.AddWithValue("@AddedBy", product.AddedBy);
+                            insertCmd.Parameters.AddWithValue("@AddedDate", product.AddedDate);
+                            insertCmd.Parameters.AddWithValue("@AddedPc", product.AddedPc);
+                            insertCmd.Parameters.AddWithValue("@UpdatedBy", product.UpdatedBy);
+                            insertCmd.Parameters.AddWithValue("@UpdatedDate", product.UpdatedDate);
+                            insertCmd.Parameters.AddWithValue("@UpdatedPc", product.UpDatedPc);
+
+                            insertCmd.ExecuteNonQuery();
+                        }
+
+                        con.Close();
+
+                        return Ok(new { message = "product created successfully" });
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { message = "An error occurred while processing the request", error = ex.Message });
+            }
+        }
+        [HttpPut("UpdateProduct")]
+        public IActionResult UpdateProduct(Product product)
+        {
+            try
+            {
+                using (SqlConnection con = new SqlConnection(ConnectionString))
+                {
+                    con.Open();
+                    SqlCommand checkCmd = new SqlCommand("SELECT COUNT(*) FROM Products WHERE ProductId = @ProductId", con);
+                    checkCmd.Parameters.AddWithValue("@ProductId", product.ProductId);
+                    int count = (int)checkCmd.ExecuteScalar();
+
+                    if (count == 0)
+                    {
+                        return NotFound(new { message = $"Product with ProductName {product.ProductName} not found" });
+                    }
+                    using (SqlCommand updateCmd = new SqlCommand("UPDATE Products SET ProductName = @ProductName, ProductDescription = @ProductDescription, Status = @Status, Price = @Price, CategoryId = @CategoryId, UomId = @UomId, UpdatedBy = @UpdatedBy, UpdatedDate = @UpdatedDate, UpdatedPc = @UpdatedPc WHERE ProductId = @ProductId", con))
+                    {
+                        updateCmd.Parameters.AddWithValue("@ProductId", product.ProductId);
+                        updateCmd.Parameters.AddWithValue("@ProductName", product.ProductName);
+                        updateCmd.Parameters.AddWithValue("@ProductDescription", product.ProductDescription ?? (object)DBNull.Value);
+                        updateCmd.Parameters.AddWithValue("@Status", product.Status);
+                        updateCmd.Parameters.AddWithValue("@Price", product.Price ?? (object)DBNull.Value);
+                        updateCmd.Parameters.AddWithValue("@CategoryId", product.CategoryId ?? (object)DBNull.Value);
+                        updateCmd.Parameters.AddWithValue("@UomId", product.UomId ?? (object)DBNull.Value);
+                        updateCmd.Parameters.AddWithValue("@UpdatedBy", product.UpdatedBy);
+                        updateCmd.Parameters.AddWithValue("@UpdatedDate", DateTime.Now);
+                        updateCmd.Parameters.AddWithValue("@UpdatedPc", product.UpDatedPc);
+
+                        updateCmd.ExecuteNonQuery();
+                    }
+
+                    return Ok(new { message = $"Product with ProductId {product.ProductName} updated successfully" });
+                }
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { message = "An error occurred while processing the request", error = ex.Message });
+            }
+        }
+        [HttpDelete("DeleteProduct/{productId}")]
+        public IActionResult DeleteProduct(int productId)
+        {
+            try
+            {
+                using (SqlConnection con = new SqlConnection(ConnectionString))
+                {
+                    con.Open();
+                    SqlCommand checkCmd = new SqlCommand("SELECT COUNT(*) FROM Products WHERE ProductId = @ProductId", con);
+                    checkCmd.Parameters.AddWithValue("@ProductId", productId);
+                    int count = (int)checkCmd.ExecuteScalar();
+                    if (count == 0)
+                    {
+                        return NotFound(new { message = $"Product  not found" });
+                    }
+                    using (SqlCommand deleteCmd = new SqlCommand("DELETE FROM Products WHERE ProductId = @ProductId", con))
+                    {
+                        deleteCmd.Parameters.AddWithValue("@ProductId", productId);
+                        deleteCmd.ExecuteNonQuery();
+                    }
+                    return Ok(new { message = $"Product Data deleted successfully" });
+                }
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { message = "An error occurred while processing the request", error = ex.Message });
+            }
+        }
+
+    }
+}
---
+++ FarmProject/Models/Product.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel;
+
+namespace FarmProject.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public string? ProductDescription { get; set; }
+        public bool Status { get; set; } = true;
+        public double? Price { get; set; }
+        public int? CategoryId { get; set; }
+        public int? UomId { get; set; }
+        public string? AddedBy { get; set; } = "appUser";
+        public DateTime? AddedDate { get; set; } = DateTime.Now;
+        public string? AddedPc { get; set; } = "Default App User";
+        public string? UpdatedBy { get; set; } = "appUser";
+        public DateTime? UpdatedDate { get; set; } = DateTime.Now;
+        public string? UpDatedPc { get; set; } = "Default App User";
+    }
+}
